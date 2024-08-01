d0d36ce21f876294138e68d32f3f2ba25c6a6bd8
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-01 23:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+    var modal = document.getElementById("myModal");
+    var openBtn = document.getElementById("openModal");
+    var closeBtn = document.getElementsByClassName("close-modal")[0];
+
+    openBtn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
